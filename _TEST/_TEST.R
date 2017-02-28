e049dc63987d253e6e739e97d2f134ca92cac203
mihawk <mihawk20101@sina.com>
2017-03-01 02:21:10 +0800

    markdown updated

--- _TEST/_TEST.R
+++ _TEST/_TEST.R
@@ -7,7 +7,7 @@ if ('package:RMitekeLab' %in% package.list) {
 library(RMitekeLab)
 source('./MQ_Analystic/CLASS.mq_analystic.R')
 source('./MQ_Analystic/output.report.R')
-
+# load('./_TEST/report.rdata')
 
 files <- file.path('./_TEST/_FILES', dir('./_TEST/_FILES')) %>%
   extract(1:10)
@@ -16,7 +16,7 @@ TEST <- MQ_ANALYSTIC$new()
 TEST$add.files(files)
 TEST$set('selected.index', 2)
 time1 <- system.time({
-  # TEST$output.report()
-  TEST
+  TEST$output.report()
+  # TEST
 })
 print(time1)
